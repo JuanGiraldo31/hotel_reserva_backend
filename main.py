7d5b32f09154ff691ea66ff2503c530168ef537f
JuanGiraldo31 <69476362+JuanGiraldo31@users.noreply.github.com>
2020-12-14 22:04:41 -0500

    Corrected the urls

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ api = FastAPI()
 
 origins = [
     "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
-    "http://localhost", "http://localhost:8080", "https://reservahotelesminticjg.herokuapp.com/"
+    "http://localhost", "http://localhost:8080", "https://reserva-hoteles-g1m3-18.herokuapp.com"
 ]
 
 api.add_middleware(
